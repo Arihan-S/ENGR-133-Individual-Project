0105beaa44d874a7189b043aa6639d96843563df
Arihan Srirangapatnam <arihan@pal-nat186-99-144.itap.purdue.edu>
2021-11-05 15:01:01 -0400

    Initial Commmit

---
+++ master.py
@@ -0,0 +1,40 @@
+"""
+===============================================================================
+ENGR 13300 Fall 2021
+
+Program Description
+    Replace this line with a description of your program.
+
+Assignment Information
+    Assignment:     Individual Project
+    Author:         Arihan Srirangapatnam, asrirang@purdue.edu
+    Team ID:        LC5 - 15
+
+Contributor:    Name, login@purdue [repeat for each]
+    My contributor(s) helped me:
+    [x] understand the assignment expectations without
+        telling me how they will approach it.
+    [x] understand different ways to think about a solution
+        without helping me plan my solution.
+    [x] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor here as well.
+    
+ACADEMIC INTEGRITY STATEMENT
+I have not used source code obtained from any other unauthorized
+source, either modified or unmodified. Neither have I provided
+access to my code to another. The project I am submitting
+is my own original work.
+===============================================================================
+"""
+
+import math
+
+
+
+def main():
+    print("test")    
+    
+if __name__ == '__main__':
+    main()
